9a017e81bebed045cf08111248ac5ba2a0f28c78
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-06-06 10:42:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment_5._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of steps: ");
+            long n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{n} has {CountWays(n)}");
+        }
+        public static long CountWays(long n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            return CountWays(n - 1) + CountWays(n - 2);
+        }
+        
+    }
+}
